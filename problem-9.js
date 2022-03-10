93899247d3ca5fcd0657e5d9c83119a6e724b325
zishan344 <zishanahmed344@gmail.com>
2022-03-10 11:42:09 +0600

    done the prblem number 9:

--- problem-9.js
+++ problem-9.js
@@ -1,3 +1,5 @@
+সেশন স্টোরেজে একটা অবজেক্ট স্টোর করো। আবার সেই অবজেক্ট কে নিয়ে এসে অবজেক্ট হিসেবে কনসোল লগ করো
+.
 const obj = { name: "marouf", age: 24 };
 const objStr = JSON.stringify(obj);
 console.log(objStr);
