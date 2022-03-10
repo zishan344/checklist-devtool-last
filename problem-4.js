85eb8e8a66f42affcc93ab582eadf22101dcf26f
zishan344 <zishanahmed344@gmail.com>
2022-03-10 10:26:36 +0600

    done the problem number 4:

---
+++ problem-4.js
@@ -0,0 +1,4 @@
+/* ৪. alert, prompt, confirm এই তিনটা এর মধ্যে ডিফারেন্স কি। কখন কোনটা ইউজ করতে হয়। সেই জিনিসগুলা একটু দেখে ফেলো। */
+/* alert showing web page message  
+prompt showing web page and collect the user data 
+confirm showing web page and defend user bas user click conferm something will happpen . user clice cancel something will happen */
