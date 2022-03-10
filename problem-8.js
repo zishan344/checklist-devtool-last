05ffa357c3dcccebc76d20da74ae33da0ca8a4bd
zishan344 <zishanahmed344@gmail.com>
2022-03-10 11:01:49 +0600

    done the problem number 8

---
+++ problem-8.js
@@ -0,0 +1,11 @@
+// . local storage আর session storage এর মধ্যে পার্থক্য কি
+
+/* local sotorage sob somoi take jotokkon na ami jinis ta ke kete dei nai . ar session storage all time take na jotokkon ami browser a takbo totokkon tar por browser kete dile session storage odaw 
+ai tai difference
+ */
+/* convert to the english 
+localStorage have all time stor on the browser until deleted
+
+and session storage store by data temporary until using the browser
+when cut the browser session storage will be closed
+ */
