6f6515d4178efe68be1f9a3a87a33e6301d81875
zishan344 <zishanahmed344@gmail.com>
2022-03-10 10:19:28 +0600

    done the problem number 3:

---
+++ problem-3.js
@@ -0,0 +1,4 @@
+/* ৩. setTimeout এবং setInterval কিভাবে কাজ করে। এই দুইটার মধ্যে ডিফারেন্স কি */
+// difference of set time out and setInterval
+/*setTimeout function running one time . 
+ and set setInterval function running infinite until it stopped. */
