460744d75f9ff63ed8e8c93e6c945dbb69e02a72
zishan344 <zishanahmed344@gmail.com>
2022-03-10 10:38:00 +0600

    done the problem number 5

---
+++ problem-5.js
@@ -0,0 +1,10 @@
+/* একটা url এর মধ্যে কী কী পার্টস থাকে */
+// url parts list
+protocol: "https";
+hostName: "localhost";
+port: 3000;
+// oporer 3 ta ke ak sate Ble
+href = "http://localhost:3000";
+search: "?=baz";
+hash: "#bang";
+pathname: "/foo/bar";
