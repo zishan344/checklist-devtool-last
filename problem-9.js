8edacea72050fcb1d1064899932e731dd04a72b2
zishan344 <zishanahmed344@gmail.com>
2022-03-10 11:39:56 +0600

    done the problem number 9

--- problem-9.js
+++ problem-9.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const obj = { name: "marouf", age: 24 };
+const objStr = JSON.stringify(obj);
+console.log(objStr);
+localStorage.setItem("Name-List", objStr);
+const loc = localStorage.getItem("Name-List");
+console.log(JSON.parse(loc));
